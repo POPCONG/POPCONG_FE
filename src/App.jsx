8499fa1ff7c61df6cea495f02b8612e451765b1c
kim gisu <kgs9843@naver.com>
2025-08-13 15:53:56 +0900

    [Design] nav작업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Main from "./pages/Main";
+import BottomNav from "./components/BottomNav";
 function App() {
+  const location = useLocation();
+  //로그인 화면에는 nav바 숨겨야됨!
+  const hideNavPaths = ["/login"];
+  const shouldHideNav = hideNavPaths.includes(location.state?.pathname);
   return (
-    <BrowserRouter>
+    <>
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
-    </BrowserRouter>
+      {!shouldHideNav && <BottomNav />}
+    </>
   );
 }
 export default App;
